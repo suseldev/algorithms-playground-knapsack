2691a06e807e230d772db3e3dbb75e9008bf4bdb
xxSusel <xxsusel@protonmail.com>
2025-06-18 00:00:07 +0200

    Add docstrings

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ import cmd
 import algorithms
 
 def getInputInRange(prompt, start=0, end=None) -> int:
+    """
+    Prompts the user for an integer input within a specified range.
+    Args:
+        prompt (str): Message to be displayed
+        start (int, optional): Minimum acceptable value (inclusive) - default is 0.
+        end (int, optional): Maximum acceptable value. If None, no limit is enforced
+
+    Returns:
+        int: Integer input provided by the user
+
+    Raises:
+        ValueError: If start is greater than or equal to end when both are specified.
+    """
     if not end is None and start >= end:
         raise ValueError("Range beginning cannot be larger than its end!")
     givenInput = input(prompt)
@@ -20,6 +33,18 @@ def getInputInRange(prompt, start=0, end=None) -> int:
         return getInputInRange(prompt, start=start, end=end)
 
 def promptItems(n) -> dict:
+    """
+    Prompts the user to enter 'value' and 'weight' for n items.
+    Args:
+        n (int): Number of items to be entered.
+
+    Returns:
+        dict: A dictionary of items where keys are item IDs and values are dictionaries with 'value' and 'weight' keys.
+            Example: {0: {'value': 60, 'weight': 10}, ...}
+    
+    Raises:
+        ValueError: If n is less than or equal to zero.
+    """
     itemList = {}
     if n <= 0:
         raise ValueError("Invalid items count!")
